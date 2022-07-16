e2fc89d410619f2b8afa1830d527dfe9401b989a
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-07-16 18:42:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+## This is a custom template
+
+it includes the basic todo app from SvelteKit and Tailwind preconfigured.
+
+```bash
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
+
+> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main class="flex-1 flex flex-col p-4 w-full max-w-6xl mx-auto my-0 box-border">
+	<slot />
+</main>
+
+<footer class="flex flex-col justify-center items-center p-4">
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
+
+<style>
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
